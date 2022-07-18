fac109caa963571fd80c2087aff22a8572559d59
codewithumar <codewithumar@gmail.com>
2022-07-18 23:35:08 +0500

    v8.4.2

--- FOS/GUI/Admin_GUI.Designer.cs
+++ FOS/GUI/Admin_GUI.Designer.cs
@@ -38,30 +38,21 @@
             this.txt_itemPrice = new System.Windows.Forms.TextBox();
             this.txt_itemname = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-<<<<<<< HEAD
             this.gdv_orderlist = new System.Windows.Forms.DataGridView();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gdv_orderlist)).BeginInit();
-=======
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
->>>>>>> a9fb7f9c9bc7620c58d569fbf65e2f6e0564eb70
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(0, 2);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1255, 500);
+            this.tabControl1.Size = new System.Drawing.Size(1434, 667);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -73,21 +64,19 @@
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.txt_itemPrice);
             this.tabPage1.Controls.Add(this.txt_itemname);
-            this.tabPage1.Location = new System.Drawing.Point(4, 24);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage1.Size = new System.Drawing.Size(793, 423);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Size = new System.Drawing.Size(1426, 634);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Add Item";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(280, 177);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(320, 236);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 22);
+            this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 7;
             this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = true;
@@ -96,18 +85,18 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(145, 124);
+            this.label3.Location = new System.Drawing.Point(166, 165);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 15);
+            this.label3.Size = new System.Drawing.Size(75, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "Item Price";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(145, 92);
+            this.label2.Location = new System.Drawing.Point(166, 123);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 15);
+            this.label2.Size = new System.Drawing.Size(74, 20);
             this.label2.TabIndex = 5;
             this.label2.Text = "Item Type";
             // 
@@ -118,84 +107,61 @@
             "Desi",
             "Italian",
             "Arabic"});
-            this.cmb_itemtype.Location = new System.Drawing.Point(241, 86);
-            this.cmb_itemtype.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmb_itemtype.Location = new System.Drawing.Point(275, 115);
             this.cmb_itemtype.Name = "cmb_itemtype";
-            this.cmb_itemtype.Size = new System.Drawing.Size(133, 23);
+            this.cmb_itemtype.Size = new System.Drawing.Size(151, 28);
             this.cmb_itemtype.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(145, 51);
+            this.label1.Location = new System.Drawing.Point(166, 68);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 15);
+            this.label1.Size = new System.Drawing.Size(83, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Item Name";
             // 
             // txt_itemPrice
             // 
-            this.txt_itemPrice.Location = new System.Drawing.Point(241, 119);
-            this.txt_itemPrice.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_itemPrice.Location = new System.Drawing.Point(275, 159);
             this.txt_itemPrice.Name = "txt_itemPrice";
-            this.txt_itemPrice.Size = new System.Drawing.Size(133, 23);
+            this.txt_itemPrice.Size = new System.Drawing.Size(151, 27);
             this.txt_itemPrice.TabIndex = 2;
             // 
             // txt_itemname
             // 
-            this.txt_itemname.Location = new System.Drawing.Point(241, 49);
-            this.txt_itemname.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_itemname.Location = new System.Drawing.Point(275, 65);
             this.txt_itemname.Name = "txt_itemname";
-            this.txt_itemname.Size = new System.Drawing.Size(133, 23);
+            this.txt_itemname.Size = new System.Drawing.Size(151, 27);
             this.txt_itemname.TabIndex = 0;
             // 
             // tabPage2
             // 
-<<<<<<< HEAD
             this.tabPage2.Controls.Add(this.gdv_orderlist);
-            this.tabPage2.Location = new System.Drawing.Point(4, 24);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-=======
-            this.tabPage2.Controls.Add(this.dataGridView1);
             this.tabPage2.Location = new System.Drawing.Point(4, 29);
->>>>>>> a9fb7f9c9bc7620c58d569fbf65e2f6e0564eb70
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage2.Size = new System.Drawing.Size(1247, 472);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(1426, 634);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "View orders";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-<<<<<<< HEAD
             // gdv_orderlist
             // 
             this.gdv_orderlist.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gdv_orderlist.Location = new System.Drawing.Point(2, 2);
-            this.gdv_orderlist.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gdv_orderlist.Location = new System.Drawing.Point(2, 3);
             this.gdv_orderlist.Name = "gdv_orderlist";
             this.gdv_orderlist.RowHeadersWidth = 51;
             this.gdv_orderlist.RowTemplate.Height = 29;
-            this.gdv_orderlist.Size = new System.Drawing.Size(1242, 254);
+            this.gdv_orderlist.Size = new System.Drawing.Size(1419, 339);
             this.gdv_orderlist.TabIndex = 0;
-=======
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(2, 3);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(586, 406);
-            this.dataGridView1.TabIndex = 0;
->>>>>>> a9fb7f9c9bc7620c58d569fbf65e2f6e0564eb70
             // 
             // Admin_GUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1251, 503);
+            this.ClientSize = new System.Drawing.Size(1430, 671);
             this.Controls.Add(this.tabControl1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Admin_GUI";
             this.Text = "Admin_GUI";
             this.Load += new System.EventHandler(this.Admin_GUI_Load);
@@ -203,11 +169,7 @@
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
-<<<<<<< HEAD
             ((System.ComponentModel.ISupportInitialize)(this.gdv_orderlist)).EndInit();
-=======
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
->>>>>>> a9fb7f9c9bc7620c58d569fbf65e2f6e0564eb70
             this.ResumeLayout(false);
 
         }
@@ -224,10 +186,7 @@
         private Button button1;
         private Label label3;
         private Label label2;
-<<<<<<< HEAD
+
         private DataGridView gdv_orderlist;
-=======
-        private DataGridView dataGridView1;
->>>>>>> a9fb7f9c9bc7620c58d569fbf65e2f6e0564eb70
     }
 }
